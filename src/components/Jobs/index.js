f0b5e8bcc848cb2527d0d68a6d8f7668288f5ee6
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-09-13 01:15:24 -0600

    began logic for tracking jobs in Jobs component

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -1,31 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import testJobs from '../../assets/js/jobs';
 
 function Jobs () {
+
+    const [jobStatus, setJobStatus] = useState();
+
+    function updateStatus(e) {
+        console.log(e.target.value);
+    }
 
     return(
         <section>
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-12 col-md-4">
-                        <h2>Need to Apply</h2>
+                        <h2 className="section-title glowing">Need to Apply</h2>
                         {testJobs.map((job) => (
                             (job.status === "need-apply" &&
-                            <h3>{job.name}</h3>)
+                            <div key={job.name} className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">{job.name}</h5>
+                                    <button className="btn btn-primary" value="applied" onClick={updateStatus}>Applied!</button>
+                                </div>
+                          </div>
+                          )
                         ))}
                     </div>
                     <div className="col-12 col-md-4">
-                        <h2>Waiting for Response</h2>
+                        <h2 className="section-title glowing">Waiting for Response</h2>
                         {testJobs.map((job) => (
                             (job.status === "applied" &&
-                            <h3>{job.name}</h3>)
+                            <div key={job.name} className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">{job.name}</h5>
+                                    <button className="btn btn-primary" value="interview" onClick={updateStatus}>Interview Scheduled!</button>
+                                </div>
+                          </div>
+                          )
                         ))}
                     </div>
                     <div className="col-12 col-md-4">
-                        <h2>Interview Scheduled</h2>
+                        <h2 className="section-title glowing">Interview Scheduled</h2>
                         {testJobs.map((job) => (
                             (job.status === "interview" &&
-                            <h3>{job.name}</h3>)
+                            <div key={job.name} className="card">
+                                <div className="card-body">
+                                     <h5 className="card-title">{job.name}</h5>
+                                    <button className="btn btn-primary" value="interview-completed" onClick={updateStatus}>Interview Completed!</button>
+                                </div>
+                          </div>
+                          )
                         ))}
                     </div>
                 </div>
